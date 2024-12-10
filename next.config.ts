ada29e56635fcd8ad64605a5da217a20b15c6073
scato3 <tlsgustn93@naver.com>
2024-12-10 20:20:37 +0900

    fix: rewrites 제거

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["cdn.upbit.com"], // 외부 이미지 도메인 추가
   },
-
-  async rewrites() {
-    return [
-      {
-        source: "/api/v1/:path*",
-        destination: "https://api.upbit.com/v1/:path*",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
